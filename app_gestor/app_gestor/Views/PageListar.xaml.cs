1dbc52d25419c04d72d454c799a2c18ac8e81af6
Aluno <Aluno@SNCTM202LABF10>
2024-02-21 20:45:33 -0300

    Adicionar arquivos de projeto.

---
+++ app_gestor/app_gestor/Views/PageListar.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using app_gestor.Model;
+using app_gestor.Service;
+
+namespace app_gestor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageListar : ContentPage
+    {
+        public PageListar()
+        {
+            InitializeComponent();
+            AtualizarLista();
+        }
+        public void AtualizarLista()
+        {
+            String titulo = "";
+            if (txtNome.Text != null) titulo = txtNome.Text;
+            ServiceDBEmployee dbFuncionario = new ServiceDBEmployee(App.DbPath);
+            if (swFavorito.IsToggled)
+            {
+                ListaFuncionario.ItemsSource = dbFuncionario.Localizar(titulo, true);
+            }
+            else
+            {
+                ListaFuncionario.ItemsSource = dbFuncionario.Localizar(titulo);
+            }
+        }
+
+        private void ListaFuncionario_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ModelEmployee funcionario = (ModelEmployee)ListaFuncionario.SelectedItem;
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new PageCadastrar(funcionario));
+        }
+
+        private void swFavorito_Toggled(object sender, ToggledEventArgs e)
+        {
+            AtualizarLista();
+        }
+
+        private void btLocalizar_Clicked(object sender, EventArgs e)
+        {
+            AtualizarLista();
+        }
+    }
+}
